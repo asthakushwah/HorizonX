c01f18050761fc3c6e1fc92e4a65f80e50aec972
Astha Kushwah <asthakushwah@Asthas-MacBook-Pro.local>
2026-08-31 01:01:18 +0530

    update backend configuration

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+const connectDB = require("./config/db");
 const app = require("./app");
 
-module.exports = app;
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
