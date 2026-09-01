35464f81f151f73466bc6e07e9d0347a790b7f74
Astha Kushwah <asthakushwah@Asthas-MacBook-Pro.local>
2026-09-01 23:09:05 +0530

    Fix Earth image paths

--- client/src/assets/earthImagesData.js
+++ client/src/assets/earthImagesData.js
@@ -1,6 +1,68 @@
-export const earthImages = Array.from({ length: 12 }, (_, i) => ({
-  id: `earth-${i + 1}`,
-  date: `2026-0${(i % 9) + 1}-2${i % 9}`,
-  caption: `Earth full-disc capture — orbit pass ${i + 1}`,
-  img: `https://picsum.photos/seed/earth${i}/700/700`,
-}));
+import earth1 from "./earth/earth1.png";
+import earth2 from "./earth/earth2.png";
+import earth3 from "./earth/earth3.png";
+import earth4 from "./earth/earth4.png";
+import earth5 from "./earth/earth5.png";
+import earth6 from "./earth/earth6.png";
+import earth7 from "./earth/earth7.png";
+import earth8 from "./earth/earth8.png";
+import earth9 from "./earth/earth9.png";
+import earth10 from "./earth/earth10.png";
+import earth11 from "./earth/earth11.png";
+import earth12 from "./earth/earth12.png";
+import earth13 from "./earth/earth13.png";
+
+export const earthImages = [
+  {
+    title: "Earth — Africa",
+    image: earth1,
+  },
+  {
+    title: "Earth — Pacific Ocean",
+    image: earth2,
+  },
+  {
+    title: "Earth — India & Australia",
+    image: earth3,
+  },
+  {
+    title: "Earth — Departure View",
+    image: earth4,
+  },
+  {
+    title: "Earth — South America",
+    image: earth5,
+  },
+  {
+    title: "Earth — Africa & Arabia",
+    image: earth6,
+  },
+  {
+    title: "Earth — Eastern Australia",
+    image: earth7,
+  },
+  {
+    title: "Earth — Antarctica",
+    image: earth8,
+  },
+  {
+    title: "Earth & Moon",
+    image: earth9,
+  },
+  {
+    title: "Earth — Western Hemisphere",
+    image: earth10,
+  },
+  {
+    title: "Earth — Cloud Systems",
+    image: earth11,
+  },
+  {
+    title: "Earth — DSCOVR",
+    image: earth12,
+  },
+  {
+    title: "Earth — Night Orbit",
+    image: earth13,
+  },
+];
